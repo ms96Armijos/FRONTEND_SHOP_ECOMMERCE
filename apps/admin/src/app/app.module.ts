098d0d41a6b352f1460cb296052911671df94c84
Steeven Michael Armijos Bravo <ms96armijos@gmail.com>
2023-10-30 19:58:55 -0500

    componentes compartidos

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
+    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent],
     imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })],
     providers: [],
     bootstrap: [AppComponent]
